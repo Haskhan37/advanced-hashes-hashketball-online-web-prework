d13f3e6035420d5174632a0c2de492c8728ee24d
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-02 20:47:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,6 @@
 require 'pry'
 
-def num_points_scored(player_search)
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:player_name] == player_search
-        return player[:points]
-      end
-    end
-  end
-end
+
 
 def shoe_size(name)
   game_hash.each do |team, team_info|
